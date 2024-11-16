308718f2bcf48ae8a05bbb75272fa28a56bfd5a6
Dillon Barnes <amber@transcental.dev>
2024-11-16 23:12:51 +0000

    do stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+    <title>amber's abode</title>
+</svelte:head>
+
+<h1>amber's abode</h1>
+<p>Personal. Fun. Wacky. that's what the internet is all about. but honestly, it kinda just feels like a chore a lot of the time. i don't want that. ghis domain isn't going to be a chore, i want to enjoy working on it. i want it to be a reflection of me, what i can do and who i am.</p>
+<p>ANYWAY! welcome to my abode!!!</p>
+<h2>who am i?</h2>
+<p>oh, i never introduced myself! i'm sorry, that was rude of me. i'm amber and i'm a 17 y/o girl living in leicester, england. occasionally i make web pages (like this one), but i prefer to make apps for chat platforms like slack and discord, random python scripts, backends or desktop apps with tauri</p>
